36b38b8a5d506650084a3159eba67b451b3c8611
jiangtao <410518072@qq.com>
2025-06-06 21:51:43 +0800

    模板

--- src/main/java/com/example/xq/NameUtil.java
+++ src/main/java/com/example/xq/NameUtil.java
@@ -14,7 +14,12 @@ public class NameUtil {
 
 
     // 中文棋谱从右到左
-    private static final String COLUMN_LETTERS_CN = "九八七六五四三二一";
+    private static final String COLUMN_LETTERS_CN = "一二三四五六七八九";
+    private static final Map<Character, Character> PIECE_MAP = Map.of(
+            'r', '车', 'n', '马', 'c', '炮',
+            'b', '象', 'a', '士', 'k', '帅', 'p', '兵'
+    );
+
 
     public static String convertToChineseNotation(String[][] board, String move) {
         if (move.length() != 4) {
@@ -59,8 +64,9 @@ public class NameUtil {
         int x2 = move.charAt(2) - 'a';
         int y2 = move.charAt(3) - '0';
 
-        char x1cn = COLUMN_LETTERS_CN.charAt(x1);
-        char x2cn = COLUMN_LETTERS_CN.charAt(x2);
+        // 中文（中文坐标从右到左，从1开始）
+        char x1cn = COLUMN_LETTERS_CN.charAt(COLUMN_LETTERS_CN.length() - x1 - 1);
+        char x2cn = COLUMN_LETTERS_CN.charAt(COLUMN_LETTERS_CN.length() - x2 - 1);
 
         // 平
         if (y1 == y2) { // 平，      H2-E2  炮二平五
@@ -68,12 +74,13 @@ public class NameUtil {
         }
 
 
-        String dir = y2 > y1 ? "进" : "退";
+        String dir = y2 < y1 ? "进" : "退";
         int step = Math.abs(y2 - y1);
+        char stepCn = COLUMN_LETTERS_CN.charAt(step - 1);
 
         // 进退， 判断字母相同
         if (x1 == x2) { //     map.put("E2-E6", "炮五进四");
-            return x1cn + dir + step;
+            return x1cn + dir + stepCn;
         }
 
         // 如果是否斜着走，最后一个字为目标列号
@@ -81,16 +88,7 @@ public class NameUtil {
     }
 
     public static char getChinesePieceName(char pieceType) {
-        return switch (pieceType) {
-            case 'r' -> '车';
-            case 'n' -> '马';
-            case 'c' -> '炮';
-            case 'b' -> '象';
-            case 'a' -> '士';
-            case 'k' -> '帅';
-            case 'p' -> '兵';
-            default -> pieceType;
-        };
+        return PIECE_MAP.getOrDefault(pieceType, pieceType);
     }
 
     public static void main(String[] args) {
