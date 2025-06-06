d3553b3b6eb32ea448cbe4f61bbb49006e946eed
jiangtao <jiangtao.cctv@qq.com>
2025-06-06 16:26:34 +0800

    部署

--- src/main/java/com/example/xq/engine/PikafishProcessHandler.java
+++ src/main/java/com/example/xq/engine/PikafishProcessHandler.java
@@ -14,24 +14,38 @@ public class PikafishProcessHandler {
     private BufferedWriter writer;
 
     // 大多数情况下，引擎速度：vnni512>avx512>avx512f>avxvnni>bmi2>avx2>sse41-popcnt>ssse3 棋友根据自己的CPU选择相应的引擎
-    private static  String[] FILE_NAMES = {"vnni512","avx512","avx512f","avxvnni","bmi2","avx2","sse41-popcnt","ssse3"};
+    private static String[] FILE_NAMES = {"vnni512", "avx512", "avx512f", "avxvnni", "bmi2", "avx2", "sse41-popcnt", "ssse3"};
 
     public void startEngine(File dir) throws IOException {
+        File file = findFile(dir);
+
+        log.info("使用引擎" + file.getAbsolutePath());
+
+        engineProcess = Runtime.getRuntime().exec(file.getAbsolutePath());
+        reader = new BufferedReader(new InputStreamReader(engineProcess.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(engineProcess.getOutputStream()));
+
+        // 发送初始化命令
+        sendCommand("uci");
+        waitForResponse("uciok");
+    }
+
+    private File findFile(File dir) {
         boolean win = SystemUtil.getOsInfo().isWindows();
 
         for (String fileName : FILE_NAMES) {
             fileName = "pikafish-" + fileName;
-            if(win){
-                fileName = fileName +".exe";
+            if (win) {
+                fileName = fileName + ".exe";
             }
             File file = new File(dir, fileName);
-            if(!file.exists()){
+            if (!file.exists()) {
                 log.warn("软件不存在 {}", file.getAbsolutePath());
                 continue;
             }
             String help = RuntimeUtil.execForStr(file.getAbsolutePath(), "help");
-            log.info("help: {}",help);
-            if(StrUtil.isEmpty(help)){
+            log.info("help: {}", help);
+            if (StrUtil.isEmpty(help)) {
                 log.warn("软件无法运行 {}", file);
                 continue;
             }
@@ -39,19 +53,15 @@ public class PikafishProcessHandler {
 
             log.info("使用引擎" + file.getAbsolutePath());
 
-            engineProcess = Runtime.getRuntime().exec(file.getAbsolutePath());
-            reader = new BufferedReader(new InputStreamReader(engineProcess.getInputStream()));
-            writer = new BufferedWriter(new OutputStreamWriter(engineProcess.getOutputStream()));
 
-            // 发送初始化命令
-            sendCommand("uci");
-            waitForResponse("uciok");
-            break;
+            return file;
         }
+
+        return null;
     }
 
     public void sendCommand(String command) throws IOException {
-        log.info("发送命令行: {}",command);
+        log.info("发送命令行: {}", command);
         writer.write(command + "\n");
         writer.flush();
     }
@@ -59,7 +69,7 @@ public class PikafishProcessHandler {
     public String waitForResponse(String expected) throws IOException {
         String line;
         while ((line = reader.readLine()) != null) {
-            log.info("命令行响应: {}",line);
+            log.info("命令行响应: {}", line);
             if (line.startsWith(expected)) {
                 return line;
             }
