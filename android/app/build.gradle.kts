57d7bb59d753b810dc2f097ba3e1b7e612d3482b
jiangood <jiangood@outlook.com>
2026-06-26 14:20:19 +0800

    chore: bump to v2.2, fix CI workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.jiangood.xq"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "2.2"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
