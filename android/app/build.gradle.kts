41539bfceb5037d0821e39167a03137574ca1b30
jiangood <410518072@qq.com>
2026-06-27 16:02:25 +0800

    chore: bump version to 2.27

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.jiangood.xq"
         minSdk = 34
         targetSdk = 34
-        versionCode = 31
-        versionName = "2.26"
+        versionCode = 32
+        versionName = "2.27"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
