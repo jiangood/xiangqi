46b4a4d33ff7bb5bb8001feb9b483db231d6504b
jiangood <jiangood@outlook.com>
2026-06-28 12:15:54 +0800

    chore: bump version to 3.0.6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.jiangood.xq"
         minSdk = 34
         targetSdk = 34
-        versionCode = 40
-        versionName = "3.0.5"
+        versionCode = 41
+        versionName = "3.0.6"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
