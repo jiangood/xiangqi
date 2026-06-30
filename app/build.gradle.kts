1d32d983d6bb659bb4cca2c6fc3ef270d0138a5d
jiangood <jiangood@outlook.com>
2026-07-01 01:14:57 +0800

    bump to 7.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.jiangood.xq"
         minSdk = 34
         targetSdk = 34
-        versionCode = 102
-        versionName = "7.4.0"
+        versionCode = 103
+        versionName = "7.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             // debug 保留所有 ABI 以支持模拟器测试
